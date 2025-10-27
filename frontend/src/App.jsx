6fd1b1bf7c8e55ca53b5d1202ed99a3ef2e6287f
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-10-27 12:32:49 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='contaner text-red'>
+      dfwfc
     </div>
   )
-}
+} 
 
-export default App
+export default App        
+ 
