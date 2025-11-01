9d12b717808e745f8a3b6943fc5eb8006d23437c
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-11-01 18:51:02 +0530

    updated

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Collection = () => {
+  const { products } = useContext(ShopContext)
   return (
-    <div>
-      
+    <div className='flex flex-col sm:flex-row sm:gap-10 pt-10 border-t border-gray-200'>
+        <div className='min-w-60'>
+          <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+          <div className={``}>
+
+          </div>
+        </div>
     </div>
   )
 }
