27bd8486b3281f8707bed8b5781825267a7b194e
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-11-01 12:31:59 +0530

    updated

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const OurPolicy = () => {
-  return (
+  return ( 
     <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-10 pb-20 text-sm sm:text-sm lg:text-base text-gray-700'>
         <div className=''>
             <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="" />
