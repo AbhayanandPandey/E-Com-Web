c40c103fad8ad0f7b29a6bd5da7cbc32958352be
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-11-03 19:19:32 +0530

    updated

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     const [imgLoaded, setImgLoaded] = useState(false)
 
     return (
-        <div className='flex flex-col sm:flex-row border border-gray-300 shadow:sm'>
+        <div className='flex flex-col sm:flex-row border border-gray-300'>
             <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
                 <div className='text-[#414141]'>
                     <div className='flex items-center gap-2'>
