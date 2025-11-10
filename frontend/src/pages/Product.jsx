c9dac8ae5a0bec719e468cc59877abed53b02c56
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-11-10 19:16:56 +0530

    updated

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
-  return (
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext)
+  const [image, setImage] = useState('')
+  const [productData, setProductData] = useState(false)
+  const fetchProductData = async () => {
+    products.map((i) => {
+      if (i._id === productId) {
+        setProductData(i)
+        setImage(i.image[0])
+        return null
+      }
+    })
+  }
+
+  useEffect(() => {
+    fetchProductData()
+  }, [productId])
+  return productData ? (
     <div>
-      
+
     </div>
-  )
+  ) : <div className=' opacity-0'></div>
 }
 
 export default Product
